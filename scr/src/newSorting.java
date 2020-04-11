600958645d89b0048d92e310c55aafd5f27f9356
sergioavalos <geo.avalos1@gmail.com>
2020-04-10 20:32:46 -0600

    Final product

--- scr/src/newSorting.java
+++ scr/src/newSorting.java
@@ -1,104 +1,128 @@
 public class newSorting {
 
-    newSorting(){}
 
-    newSorting(int[] A , int size){
-    if(A.length <= size){
-        quickSort(A);
-    }else{
-        int mid = A.length/2;
+   void newSorting(int[] A , int size){
+        int mid = A.length/2; //mid i used to see how big LH[] and Rh[] will be.
+       if(A.length <= size){
+           quickSort(A);
+
+       }else{
+
         int[] LH = new int[mid];
-        int[] RH = new int[A.length -mid];
-        new newSorting(LH,size);
-        new newSorting(RH,size);
-        mergeSortedHalves(A,LH,RH);
+        int[] RH = new int[A.length - mid];
+        for(int i = 0; i < mid; i++){ //For loop takes data from A[] and puts them on Rh and LH
+            LH[i] = A[i];
+            RH[i] = A[i+mid];
+        }
+           if(A.length % 2 == 1){ //IF A[] is an odd length size this will make sure all data is distributed correctly.
+               RH[mid] = A[A.length -1]; //Number not covered in for loop.
+           }
+        newSorting(LH,size);
+        newSorting(RH,size);
+
+        mergeSortedHalves(A,LH,RH); //Calls Merge method to execute final sorted array.
     }
+
     }
 
     private void quickSort(int [] A){//this is where we call the helper method for multiple passes.
         int left = 0;
         int right = A.length -1;
-       int x =  quickSort(A,left,right);
+        int end = A.length;
+       quickSort(A,left,right); //This just calls once and helper method will solve array.
 
     }
-    private int quickSort(int [] A , int left , int right){ //this is only 1 pass of quicksort
+    private void quickSort(int [] A , int left , int right){ //this is only 1 pass of quicksort.
         int pivot = A[left];
         int start = left;
         int end = right;
-    while(left<= right){
-        left++;
+        if(A.length == 2){ //Base case that takes care of length 2 arrays.
+            if(A[0] > A[1]){
+            int temp = pivot;
+            A[0]= A[right];
+            A[right] = temp;
+            return;}else{
+                return;
+            }
+        }
+        if(A.length == 1){//Base case that takes care of only length 1 arrays.
+            return;
+        }
+
+
+        while(A[left] <= pivot &&  left< A.length -1){//checks if pivot is greater than numbers after pivot and stops accordingly.
+            left++;
+        }
+
+    while(A[right] > pivot && right > 0){//checks if pivot is less than numbers after pivot and stops accordingly.
+
+        if(left != A.length -1){
+            right--;}
     }
-    while(A[right] > pivot){
-        right--;
+    if(right <= left){ //Upon stopping point swaps numbers. In this case pivot with number stopped on right.
+        int temp = pivot;
+        A[0]= A[right];
+        A[right] = temp;
+
     }
-    if(left <= right){
-        int temp = A[right];
+    else{//This deals with other instance where 2 numbers other than pivot need to be swapped.
+        int temp = A[left];
         A[left]= A[right];
         A[right] = temp;
-        left++;
-        right--;
+
     }
-    if(start < left -1){
-        quickSort(A,start,left-1);
+        if(left == 1 && right == 0){ //I noticed pattern where upon a sorted array left is always 1 and right is 0.
+            return;//So this stops method when this is true.
+        }else {
+            quickSort(A, start, end); //this just makes sure if array isn't sorted then it will keep repeating method.
         }
-    if(right > left){
-        quickSort(A,left, end);
-    }
-    return left;
     }
     private void mergeSortedHalves(int[] A, int[] LH, int[] RH){
-
-//        int x =(A.length -1)/2;
-//        for(int i = 0; i < x; i++){
-//            A[i] = LH[i];
-//            A[i+LH.length] = RH[i];
-//
-//        }
         int left = 0;
         int right = 0;
-        int place =0;
-        while(left != LH.length-1 && right != RH.length -1){
+        int place =0; //Indicates where in array A will the data be replaced.
+        while(place != A.length ){ //While loop does not stop until all spaces in array A are filled.
 
-            if(LH[left] == RH[right]){
-                if(left == LH.length -1){
-                    A[place] = RH[right];
-                    right++;
-                    place++;
+            if(left == LH.length || right == RH.length ) { //Makes sure that if LH is completely navigated through then the
+                if (left == LH.length) {                  //rest of the numbers will come from RH.
+                      left = left-1;
+                    while (place != A.length) {
+                        A[place] = RH[right];
+                        right++; //increases the posotion where the number are replaced. ex. RH[0],RH[1]..
+                        place++; //increases the posotion where the number are replaced. ex. A[0],A[1]..
+                    }
+                    return; //Stops method upon sorted array.
                 }
-                if(right == RH.length -1){
+                if (right == RH.length) { //Makes sure that if RH is completely navigated through then the
+                    right = right -1;     //rest of the numbers will come from LH.
+                    while (place != A.length) {
+                        A[place] = LH[left];
+                        left++; //increases the posotion where the number are replaced. ex. LH[0],LH[1]..
+                        place++;
+                    }
+                    return; //Stops method upon sorted array.
+                }
+            }else {
+
+                if (LH[left] == RH[right]) { //if elements are equal then both are used and put into A[].
                     A[place] = LH[left];
+                    place++;
+                    A[place] = RH[right];
+                    left++;
+                    right++;
+                    place++;
+
+                }
+                else if(LH[left] > RH[right]) { //If LH[] is greater than RH[] then LH is put into array A.
+                    A[place] = RH[right];       //While RH[] has to go through while loop again to be compared to other number.
+                    right++;
+                    place++;
+                }else{
+                    A[place] = LH[left]; //Same as above but just inverse.
                     left++;
                     place++;
                 }
-
-                A[place] = LH[left];
-                place++;
-                A[place] = RH[right];
-                left++;
-                right++;
-                place++;
-            }
-            if(LH[left] >RH[right]){
-                if(right == RH.length-1){
-                    A[place] = RH[left];
-                    left++;
-                }else {
-                    A[place] = RH[right];
-                    right++;
-                }
-                place++;
-            }
-            if(LH[left] <  RH[right]){
-                if(left == LH.length -1){
-                    A[place] = RH[right];
-                    right++;
-                }else {
-                    A[place] = RH[left];
-                    left++;
-                }
-                place++;
             }
         }
-
     }
 }
--- scr/src/newSortingTester.java
+++ scr/src/newSortingTester.java
@@ -8,13 +8,93 @@ import org.junit.jupiter.api.Test;
 public class newSortingTester {
 
     @Test
-    void newSorting0(){
-        int [] A = {135246};
+    void newSorting0(){ //I used this test case to first see if my methods worked.
+                        // With this test case i learned i need to add Base case in helper method when only one digit is used in quicksort.
+        newSorting test = new newSorting();
+        int [] A = new int[] {2,1};
+        int size = 1;
+        test.newSorting(A,size);
+        int [] B = new int[] {1,2};
+
+        assertArrayEquals(B,A); //Upon testing the second time when adding new base case i found out everything worked.
+    }
+    @Test
+    void newSorting1(){  //Now in this test case i used a much larger array and uneven size.
+                        // Upon testing it i learned i needed to fix how to seperate LH and Rh corretly.
+        newSorting test = new newSorting();
+        int [] A = new int[] {5,3,1,6,4,2};
         int size = 3;
-        newSorting test = new newSorting(A,size);
-        int [] B = {123456};
-//        int size = 3;
-       // test.newSorting(B,size);
-        assertArrayEquals(A,B);
+        test.newSorting(A,size);
+        int [] B = new int[] {1,2,3,4,5,6};
+
+        assertArrayEquals(B,A); //Upon fixing and adjusting int mid in newSorting method it worked and i got sorted array.
+    }
+    @Test
+    void newSorting2(){
+
+        newSorting test = new newSorting(); //Now i wanted to test to see if using an even size with even A.length will work.
+        int [] A = new int[] {2,4,3,1};
+        int size = 2;
+        test.newSorting(A,size);
+        int [] B = new int[] {1,2,3,4};
+
+        assertArrayEquals(B,A); //This worked right away no adjustments needed.
+    }
+    @Test
+    void newSorting3(){
+
+        newSorting test = new newSorting(); //now i wanted to use a large array with small size to see if my methods could work.
+        int [] A = new int[] {4,3,2,1,6,5,7,8};
+        int size = 3;
+        test.newSorting(A,size);
+        int [] B = new int[] {1,2,3,4,5,6,7,8}; // Upon testing it i found out that i had to make sure that my merge method did not go out of bounds.
+                                                // Upon messing with conditionals i finally was able to get it to work when using else if.
+        assertArrayEquals(B,A);
+    }
+    @Test
+    void newSorting4(){ // Now i wanted to see how my code would act when using array with negative numbers.
+
+        newSorting test = new newSorting();
+        int [] A = new int[] {-4,3,-2,-1,6,5,-7,8};
+        int size = 3;
+        test.newSorting(A,size);
+        int [] B = new int[] {-7,-4,-2,-1,3,5,6,8};
+
+        assertArrayEquals(B,A); // It was a success and everything worked fine.
+    }
+    @Test
+    void newSorting5(){ //Now I wanted to test repeating numbers to see if merge method worked correctly.
+
+        newSorting test = new newSorting();
+        int [] A = new int[] {4,3,4,3};
+        int size = 3;
+        test.newSorting(A,size);
+        int [] B = new int[] {3,3,4,4};
+
+        assertArrayEquals(B,A); // It was a success and everything worked fine.
+    }
+    @Test
+    void newSorting6(){
+
+        newSorting test = new newSorting(); // Now i wanted to test a traditional worst case to see if code worked fine.
+        int [] A = new int[] {4,3,2,1};
+        int size = 3;
+        test.newSorting(A,size);
+        int [] B = new int[] {1,2,3,4}; //code ended up working perfect.
+
+        assertArrayEquals(B,A);
+    }
+
+    @Test
+    void newSorting7(){
+
+        newSorting test = new newSorting(); // I was curious to see how an large array with int size 1 would work.
+        int [] A = new int[] {10,9,8,7,6,5,4,3,2,1};
+        int size = 1;
+        test.newSorting(A,size);
+        int [] B = new int[] {1,2,3,4,5,6,7,8,9,10};
+
+        assertArrayEquals(B,A);//Upon testing using SOP i found out the way it is sorted is mainly using the merge method.
     }
 }
+
